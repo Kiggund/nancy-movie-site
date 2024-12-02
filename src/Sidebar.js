4104c168c5c724a424cc25951487cd9bb8e8b7c3
kiggund <abdulbasitkiggundu@gmail.com>
2024-12-02 22:48:18 +0300

    added side bar menue

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,14 +3,15 @@ import './App.css'; // Make sure to create and import your CSS file
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <ul className="sidebar-list">
-        <li className="sidebar-item">Home</li>
-        <li className="sidebar-item">New Releases</li>
-        <li className="sidebar-item">Popular</li>
-        <li className="sidebar-item">Recommended</li>
-      </ul>
-    </div>
+    <div class="sidebar">
+      <i class="left-menu-icon fas fa-search" alt="search"></i>
+      <i class="left-menu-icon fas fa-home" alt="home"></i>
+      <i class="left-menu-icon fas fa-users" alt="users"></i>
+      <i class="left-menu-icon fas fa-bookmark" alt="bookmark"></i>
+      <i class="left-menu-icon fas fa-tv" alt="tv"></i>
+      <i class="left-menu-icon fas fa-hourglass-start" alt="fa-hourglass-start"></i>
+      <i class="left-menu-icon fas fa-shopping-cart" alt="shopping-cart"></i>
+  </div>
   );
 };
 
